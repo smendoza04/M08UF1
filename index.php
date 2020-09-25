83855fbf6d18d468fdc363234a3ee34997fb4006
Sophia Mendoza <manalo.mendoza.sophia.patrice@alumnat.copernic.cat>
2020-09-25 16:00:06 +0200

    spm - commit omg

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ and open the template in the editor.
         <?php
         // put your code here
         echo "Hola tu";
-        echo"This is hard";
+        echo"This is hard omg";
         ?>
     </body>
 </html>
